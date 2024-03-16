423f46afc1c825ab45119d4654ec87876f996f25
Rhenald <renaldkarel@gmail.com>
2024-03-16 13:07:27 +0700

    feat: get albums route

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -11,4 +17,16 @@ var albums = []album{
 	{ID: "1", Title: "Bintang di Surga", Artist: "Noah", Price: 56.99},
 	{ID: "2", Title: "Kampungan", Artist: "Slank", Price: 17.99},
 	{ID: "3", Title: "Naif", Artist: "Naif", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums);
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
 }
